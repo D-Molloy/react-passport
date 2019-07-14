41e8a1b9f7ce5a520a2b894ccc4e7d962b6a2a47
Denis Molloy <denis.molloy@gmail.com>
2019-07-14 00:12:22 -0700

    front and back end setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}.`);
+});
+
+module.exports = app;
---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import NotFound from "./components/NotFound";
+
+import "./App.css";
+function Header() {
+  return <h2>This is header</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
